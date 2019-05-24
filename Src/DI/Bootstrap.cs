e50845aa1aca67443d1c933acac468a3a50415d2
Ricardo Gaefke <ricardogaefke@gmail.com>
2019-05-24 20:39:07 -0300

    entire solution

---
+++ Src/DI/Bootstrap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DI
+{
+    public class Bootstrap
+    {
+        public static void Configure(IServiceCollection services)
+        {
+            // Add your AppInsights ID here to make it globally available
+            services.AddApplicationInsightsTelemetry("");
+
+                //services.AddSingleton<Pages>();
+        }
+    }
+}
