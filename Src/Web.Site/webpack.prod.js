62be65608771d1ca90ec6c52291f3cbb56bb20ea
Ricardo Gaefke <ricardogaefke@gmail.com>
2019-06-21 01:46:19 -0300

    SSR ok with production

--- Src/Web.Site/webpack.prod.js
+++ Src/Web.Site/webpack.prod.js
@@ -1,17 +1,59 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-module.exports = merge(common, {
-  mode: 'production',
-  devtool: '#source-map',
-  entry: {
-    app: path.resolve(__dirname, './React/index.jsx'),
-    // server: path.resolve(__dirname, './React/server.jsx'),
-    // app: './React/index.jsx',
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, './wwwroot/dist'),
-  },
-});
+module.exports = () => {
+  // mode: 'production',
+  // devtool: '#source-map',
+  // entry: {
+  //   app: path.resolve(__dirname, './React/index.jsx'),
+  //   // server: path.resolve(__dirname, './React/server.jsx'),
+  //   // app: './React/index.jsx',
+  // },
+  // output: {
+  //   filename: '[name].js',
+  //   path: path.resolve(__dirname, './wwwroot/dist'),
+  // },
+
+  const clientBundle = merge(common, {
+    mode: 'production',
+    devtool: '#source-map',
+    entry: {
+      app: path.resolve(__dirname, './React/index.jsx'),
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, './wwwroot/dist'),
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'Production',
+      }),
+    ],
+  });
+
+  const serverBundle = merge(common, {
+    mode: 'production',
+    target: 'node',
+    devtool: '#source-map',
+    devServer: {
+      contentBase: './wwwwroot/dist',
+    },
+    entry: {
+      server: path.resolve(__dirname, './React/server.jsx'),
+    },
+    output: {
+      libraryTarget: 'commonjs',
+      filename: '[name].js',
+      path: path.resolve(__dirname, './React/ssr'),
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+    ],
+  });
+
+  return [clientBundle, serverBundle];
+};
