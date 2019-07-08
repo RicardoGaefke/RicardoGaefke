0feace66faa318ead8bb3d5ad61b76888edd66d8
Ricardo Gaefke <ricardogaefke@gmail.com>
2019-07-08 03:17:19 -0300

    SendGrid basic implementation

--- Src/WebJob.Email/Function.cs
+++ Src/WebJob.Email/Function.cs
@@ -1,13 +1,32 @@
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace WebJobsSDKSample
 {
   public class Functions
   {
-    public static void ProcessQueueMessage([QueueTrigger("email")] string message, ILogger logger)
+    public static async void ProcessQueueMessage([QueueTrigger("email")] string message, ILogger logger)
     {
-      logger.LogInformation(message + " - Fez upload?");
+      var apiKey = "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";
+      var client = new SendGridClient(apiKey);
+
+      var msg = new SendGridMessage();
+      msg.SetFrom(new EmailAddress("suporte@mi3dplus.com", "Suporte MI3D Plus"));
+      msg.SetSubject("Email da plataforma");
+      msg.AddContent(MimeType.Html, "<strong>Assim serão as chaves</strong>");
+      msg.AddTo(new EmailAddress("ricardogaefke@gmail.com", "Ricardo Gaefke"));
+      msg.AddCc(new EmailAddress("coachcarlosdesouza@hotmail.com", "Carlos de Souza"));
+
+      var response = await client.SendEmailAsync(msg);
+
+      string mId = response.Headers.GetValues("x-message-id").FirstOrDefault();
+      
+      logger.LogInformation(mId);
     }
   }
 }
