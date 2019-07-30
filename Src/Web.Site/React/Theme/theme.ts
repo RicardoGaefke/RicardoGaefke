9036f78c9b8dc625760e0753e76530aa0716316b
Ricardo Gaefke <ricardogaefke@gmail.com>
2019-07-30 01:31:17 -0300

    material-ui 4.3.0 + material/core/styles fixed

--- Src/Web.Site/React/Theme/theme.ts
+++ Src/Web.Site/React/Theme/theme.ts
@@ -32,7 +32,7 @@ const myTheme = (t: string = 'dark'): any => {
         light: '#757ce8',
         main: '#3f50b5',
         dark: '#002884',
-        contrastText: '#fff',
+        contrastText: '#000',
       },
       secondary: {
         light: '#ff7961',
