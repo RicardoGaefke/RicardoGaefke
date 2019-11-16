42c5461cd43e24296aedf9f7c68f7c1720baa80a
Ricardo Gaefke <ricardogaefke@gmail.com>
2019-11-15 22:37:39 -0300

    Web.CI Company form

--- Src/WebJob.Email/Function.cs
+++ Src/WebJob.Email/Function.cs
@@ -26,7 +26,7 @@ namespace MyApp.WebJob.Email
     }
     public static async void ProcessQueueMessage([QueueTrigger("email")] string message, ILogger logger)
     {
-      var apiKey = "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";
+      var apiKey = "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";
       // var apiKey = _connectionStrings.SendGrid;
       var client = new SendGridClient(apiKey);
 
