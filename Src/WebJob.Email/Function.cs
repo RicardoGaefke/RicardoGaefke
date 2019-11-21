23247fe78f99a1acc23b337f0a47771d3648e061
Ricardo Gaefke <ricardogaefke@gmail.com>
2019-11-21 12:35:49 -0300

    WebJobs.Email appsettings.json optional

--- Src/WebJob.Email/Function.cs
+++ Src/WebJob.Email/Function.cs
@@ -38,7 +38,7 @@ namespace MyApp.WebJob.Email
       {
         var configuration = new ConfigurationBuilder()
         .SetBasePath(Directory.GetCurrentDirectory())
-        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
         .AddEnvironmentVariables()
         .Build()
       ;
