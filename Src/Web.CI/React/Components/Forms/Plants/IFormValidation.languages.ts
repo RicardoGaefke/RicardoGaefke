1eb1658dd3fdd0a436fa22193e4130e5bb3edb38
LNeeevs <lucasneves.dev@gmail.com>
2019-11-23 09:36:00 -0300

    Start recuperation

--- Src/Web.CI/React/Components/Forms/Plants/IFormValidation.languages.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export interface IFormValidation {
-    Registry: number;
-    Name: string;
-    OfficialName: string;
-    Technician: string;
-    Email: string;
-    Phone: number;
-}
